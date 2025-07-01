803bce695eda369c2915d8ca5e6775f973bd1078
Suhas030 <115228993+Suhas030@users.noreply.github.com>
2025-07-01 12:58:48 +0530

    Update Login.jsx

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -22,7 +22,7 @@ function Login({ setIsAuthenticated }) {
   const handleSignIn = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('https://dresspro-back-end.onrender.com/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
